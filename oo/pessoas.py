fdf4ba8e977db30e0a69bcb98e0764898412d2cb
edusilva4 <ledamariaescola862@gmail.com>
2022-02-19 01:43:11 -0300

    Merge pull request #28 from engnogueira/engnogueira/gitpod-setup
    Fully automate dev setup with Gitpod

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,2 +1,10 @@
 class Pessoas:
-    pass
+    def cumprimentar(self):
+        return "Olá mundo cruel"
+    def trabalhar(self):
+        return "Meu trabalho é bom"
+    def calcular(self):
+        return
+
+p = Pessoas()
+print(p.cumprimentar(), p.trabalhar())
