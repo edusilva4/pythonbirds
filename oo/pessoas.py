128654fbbe5603b7d32d78f109c23e6b169ec2c8
edusilva4 <ledamariaescola862@gmail.com>
2022-03-01 16:09:27 -0300

    Merge pull request #28 from engnogueira/engnogueira/gitpod-setup
    Fully automate dev setup with Gitpod

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,16 +1,22 @@
 class Pessoas:
-    def __init__(self, nome="Null", idade ="Null"):
+    def __init__(self,*filhos, nome="Null", idade= "Null"):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return "Olá mundo cruel"
     def trabalhar(self):
         return "Meu trabalho é bom"
-    def calcular(self):
-        return
+
+
 
 p = Pessoas("Rafael",37)
 print(p.cumprimentar(), p.trabalhar())
 print(p.nome)
 p.nome = "Eduardo"
 print(p.nome,p.idade)
+
+eduardo = Pessoas(nome="Eduardo")
+julio = Pessoas(eduardo, nome="Rafael")
+for filho in julio.filhos:
+    print(filho.nome)
